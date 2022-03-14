f55f576190655051fbc084741de3bcf159f2dc48
Vinod C <vinod@legacit.com>
2022-03-14 16:30:56 +0530

    second v4.1 changes

--- src/components/WebApp/index.js
+++ src/components/WebApp/index.js
@@ -7,32 +7,32 @@ export default function WebApp(props) {
   useEffect(() => {
     const script3 = document.createElement("script");
     script3.src = "/js/swiper.min.js";
-    script3.async = true;
+    //script3.async = true;
 
     const script4 = document.createElement("script");
     script4.src = "/js/aos.js";
-    script4.async = true;
+    //script4.async = true;
 
 
     const script6 = document.createElement("script");
     script6.src = "/js/owl.carousel.min.js";
-    script6.async = true;
+    //script6.async = true;
 
     const script7 = document.createElement("script");
     script7.src = "/js/nav.js";
-    script7.async = true;
+    //script7.async = true;
 
     const script8 = document.createElement("script");
     script8.src = "/js/jquery.fancybox.min.js";
-    script8.async = true;
+    //script8.async = true;
 
     const script9 = document.createElement("script");
     script9.src = "/js/home.js";
-    script9.async = true;
+    //script9.async = true;
 
     const script10 = document.createElement("script");
     script10.src = "/js/custom.js";
-    script10.async = true;
+    //script10.async = true;
 
     document.body.appendChild(script3);
     document.body.appendChild(script4);
